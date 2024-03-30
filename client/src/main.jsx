26cd6c735c97a4e5f66fd98e3ccd5a68bd02c46b
saif01010 <saiful.12006518@lpu.in>
2024-03-30 18:41:14 +0530

    Update ESLint configuration and add new dependencies and components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import GridBackground from './components/ui/BridBackground.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+    <GridBackground>
+      <App />
+    </GridBackground>
+  </BrowserRouter>
   </React.StrictMode>,
 )
