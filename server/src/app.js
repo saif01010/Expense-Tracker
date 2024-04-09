a31b2fde9f2a1cde1607bbc7d277b0ee9b24f08b
saif01010 <saiful.12006518@lpu.in>
2024-04-09 15:08:31 +0530

    Update static file path in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -61,10 +61,10 @@ app.use('/graphql',
     {context:async({req,res})=>buildContext({req,res})
 }));
 
-app.use(express.static(path.join(__dirname, 'frontend/dist')))
+app.use(express.static(path.join(__dirname, 'client/dist')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'))
 })
 
 
